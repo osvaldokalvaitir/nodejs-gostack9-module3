30ca6f8966a3386c36ec21e653e7ff8aa52265ff
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-07-09 22:37:12 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Rocketseat' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Osvaldo Kalvaitir',
+    email: 'osvaldokalvaitir@outlook.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
