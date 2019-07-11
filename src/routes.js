030fab6d91a9720c481006bf01d91fdc5080df40
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-07-10 21:25:28 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Osvaldo Kalvaitir',
-    email: 'osvaldokalvaitir@outlook.com',
-    password_hash: '123456789',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
