e9067d4dd546edfc463652c6cc8113d3254711c9
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-07-17 13:50:31 -0300

    Update do usúario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,9 +19,30 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
