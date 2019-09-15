0cf9524e81e0fdaa4b825c61f209d1f4c04cef1b
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-09-15 13:02:04 -0300

    Campos virtuais no agendamento

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -6,6 +7,18 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
@@ -18,8 +31,7 @@ class Appointment extends Model {
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
-  };
-
+  }
 }
 
 export default Appointment;
